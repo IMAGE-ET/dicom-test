83f754ba9df1beffc2db2998de0ef4f0dcfcfde5
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2013-10-25 18:05:24 -0400

    Able to read and convert at least 1 diacom

--- dicom-test.py
+++ dicom-test.py
@@ -1,11 +1,13 @@
 __author__ = 'dhan'
 from PIL import Image
 import dicom
+import numpy
+from PIL import ImageMath
 
 import dicom
 from dicom.contrib.pydicom_PIL import show_PIL
 
-fname = "/home/dhan/data/emma/1.3.12.2.1107.5.1.4.54912.30000013032209515682800002391_4/IMG15.dcm"
+fname = "/home/dhan/data/emma/1.3.12.2.1107.5.1.4.54912.30000013032209584548400000055_0/IMG0.dcm"
 
 
 
@@ -22,9 +24,27 @@ elif bits == 16:
 else:
     raise TypeError("Don't know PIL mode for %d BitsAllocated and %d SamplesPerPixel" % (bits, samples))
 
-# PIL size = (width, height)
-size = (ds.Columns, ds.Rows)
+print mode
 
-im = Image.frombuffer(mode, size, ds.PixelData, "raw", mode, 0, 1)
-im.show()
+data = ds.PixelData
+data  = ds.pixel_array
 
+# rescale
+print "Shape: ", data.shape
+const = float(data.max())
+#print const
+data2 = numpy.divide(data, const / 255.0)
+#print data3[310]
+#print data3.shape
+print ds
+print data2[350]
+im = Image.fromarray(data,"I;16")
+im.save("some.tiff")
+out = im.point(lambda i: i * (255.0 / const))
+
+im2 = out.convert("L")
+import PIL
+from PIL import ImageOps
+im3 = ImageOps.invert(im2)
+im3.save("some2.jpg")
+Mrugendrata
